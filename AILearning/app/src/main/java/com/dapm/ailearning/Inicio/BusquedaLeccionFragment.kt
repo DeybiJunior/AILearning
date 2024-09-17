d2851a6348f193e3254d2b18493116edb13769de
Kevin Vega <kevinandresvega13@gmail.com>
2024-09-17 02:17:44 -0500

    Desarrollo del fragmento y layout de Busqueda de Leccion

--- AILearning/app/src/main/java/com/dapm/ailearning/Inicio/BusquedaLeccionFragment.kt
+++ AILearning/app/src/main/java/com/dapm/ailearning/Inicio/BusquedaLeccionFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import com.dapm.ailearning.R
+import com.google.android.material.textfield.TextInputEditText
 
 class BusquedaLeccionFragment : Fragment() {
 
@@ -17,4 +23,27 @@ class BusquedaLeccionFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_busqueda_leccion, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnGuardar = view.findViewById<Button>(R.id.btnGuardar)
+        val btnBack = view.findViewById<ImageButton>(R.id.btnBack)
+
+        // Configura el botón Guardar
+        btnGuardar.setOnClickListener {
+            // Regresa al InicioFragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, InicioFragment())
+                .addToBackStack(null)  // Opcional: Agrega la transacción a la pila de retroceso
+                .commit()
+        }
+        // Configura el botón Back
+        btnBack.setOnClickListener {
+            // Regresa al InicioFragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, InicioFragment())
+                .addToBackStack(null)  // Opcional: Agrega la transacción a la pila de retroceso
+                .commit()
+        }
+    }
 }
