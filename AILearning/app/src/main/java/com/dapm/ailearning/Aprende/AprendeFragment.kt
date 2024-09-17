4df820bea980094a283a2a9aa46540601f90092f
Kevin Vega <kevinandresvega13@gmail.com>
2024-09-17 03:01:30 -0500

    Desarrollo de Fragmento y Layout de Aprende

--- AILearning/app/src/main/java/com/dapm/ailearning/Aprende/AprendeFragment.kt
+++ AILearning/app/src/main/java/com/dapm/ailearning/Aprende/AprendeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
 import com.dapm.ailearning.R
 
 class AprendeFragment : Fragment() {
@@ -15,5 +17,36 @@ class AprendeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_aprende, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnContinuar1 = view.findViewById<Button>(R.id.btnContinuar1)
+        val btnContinuar2 = view.findViewById<Button>(R.id.btnContinuar2)
+        val btnContinuar3 = view.findViewById<Button>(R.id.btnContinuar3)
+
+        // Configurar el botón Continuar
+        btnContinuar1.setOnClickListener {
+            // Regresa al Aprende_Speack
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Aprende_speack())
+                .addToBackStack(null)  // Opcional: Agrega la transacción a la pila de retroceso
+                .commit()
+        }
+        btnContinuar2.setOnClickListener {
+            // Regresa al Aprende_Speack
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Aprende_speack())
+                .addToBackStack(null)  // Opcional: Agrega la transacción a la pila de retroceso
+                .commit()
+        }
+        btnContinuar3.setOnClickListener {
+            // Regresa al Aprende_Speack
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, Aprende_speack())
+                .addToBackStack(null)  // Opcional: Agrega la transacción a la pila de retroceso
+                .commit()
+        }
     }
 }
