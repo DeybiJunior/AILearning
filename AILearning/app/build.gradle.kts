3d7cc6a36bbed73dced16cff9f1f14a8caaf34be
Deybi Ruiz Marquina <57965724+DeybiJunior@users.noreply.github.com>
2024-10-03 22:40:37 -0500

    Update build.gradle.kts

--- AILearning/app/build.gradle.kts
+++ AILearning/app/build.gradle.kts
@@ -40,6 +40,14 @@ android {
 }
 
 dependencies {
+
+    implementation ("com.google.code.gson:gson:2.8.8")
+
+    //solicitud
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.google.firebase:firebase-database:21.0.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
